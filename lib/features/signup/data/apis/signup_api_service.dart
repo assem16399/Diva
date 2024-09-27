3d1e140a817882a37897607490dcc89ecf4931a2
Aasem <aasem.hany@gmail.com>
2024-09-27 19:06:35 +0300

    - Implement Signup api service - remove one_member_abstracts lint rule

---
+++ lib/features/signup/data/apis/signup_api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:diva/features/signup/data/apis/signup_api_constants.dart';
+import 'package:diva/features/signup/data/models/signup_request_body.dart';
+import 'package:diva/features/signup/data/models/signup_response.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'signup_api_service.g.dart';
+
+@RestApi(baseUrl: SignupApiConstants.signupBaseUrl)
+abstract class SignupApiService {
+  factory SignupApiService(Dio dio, {String baseUrl}) = _SignupApiService;
+
+  @POST(SignupApiConstants.signupEp)
+  Future<SignupResponse> signup(
+    @Body() SignupRequestBody body,
+  );
+}
