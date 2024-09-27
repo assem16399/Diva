1dab09d62cd2cd71d0a89489c40acc1fbf8b366a
Aasem <aasem.hany@gmail.com>
2024-09-27 18:08:25 +0300

    - Implement SignupRequestBody Model

---
+++ lib/features/signup/data/models/signup_request_body.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signup_request_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$SignupRequestBodyToJson(SignupRequestBody instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'password': instance.password,
+    };
