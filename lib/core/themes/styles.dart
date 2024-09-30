5c0a9587fd030f98127a0f78c7054c1458989275
asmashawky <asmaamagdy030@gmail.com>
2024-09-30 20:00:18 +0200

    styles
    adding TextStyles

--- lib/core/themes/styles.dart
+++ lib/core/themes/styles.dart
@@ -1,9 +1,29 @@
+import 'package:diva/core/themes/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'FontWeightHelper.dart';
+
 class TextStyles {
-  /// Example of defining a text style
-  ///
-  /// static TextStyle font24BlackBold = TextStyle(
-  ///   fontSize: 24.sp,
-  ///   fontWeight: FontWeightHelper.w700,
-  ///   color: Colors.black,
-  /// );
+  static TextStyle font14GreyRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: Colors.grey,
+  );
+
+  static TextStyle font14PinkRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.mainDeepPink,
+  );
+  static TextStyle font28BlackMedium = TextStyle(
+    fontSize: 28.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: Colors.black,
+  );
+  static TextStyle font20whiteRegular = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: Colors.white,
+  );
 }
