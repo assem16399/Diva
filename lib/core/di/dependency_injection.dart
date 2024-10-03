3e83883c19dd181c461c2c0cbfef470d8ae4d92b
Aasem <aasem.hany@gmail.com>
2024-10-03 13:41:07 +0300

    feat(Signup) 🎨  Initialize Signup dependency inside injection container

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,9 +1,18 @@
 import 'package:diva/core/networking/dio_factory.dart';
+import 'package:diva/features/signup/data/apis/signup_api_service.dart';
+import 'package:diva/features/signup/data/repos/signup_repos.dart';
+import 'package:diva/features/signup/logic/signup_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
-  // Dio & ApiService
-  final _ = DioFactory.getDio();
+  // Dio
+  final dio = DioFactory.getDio();
+
+  // Signup
+  getIt
+    ..registerLazySingleton<SignupApiService>(() => SignupApiService(dio))
+    ..registerLazySingleton<SignupRepo>(() => SignupRepo(getIt()))
+    ..registerFactory<SignupCubit>(() => SignupCubit(getIt()));
 }
