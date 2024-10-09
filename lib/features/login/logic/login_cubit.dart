75078570b1575429f320e3785b8ffe74484c86e8
asma <asmaamagdy030@gmail.com>
2024-10-10 01:01:25 +0200

    create login logic

--- lib/features/login/logic/login_cubit.dart
+++ lib/features/login/logic/login_cubit.dart
@@ -1,7 +1,36 @@
 import 'package:bloc/bloc.dart';
+import 'package:diva/features/login/data/repo/login_repo.dart';
+import 'package:diva/features/login/logic/login_state.dart';
+import 'package:flutter/cupertino.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  final LoginRepo _loginRepo;
+  LoginCubit(this._loginRepo) : super(const LoginState.initial());
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  void emitLoginStates() async {
+    emit(const LoginState.loading());
+    final response = await _loginRepo.login(
+      LoginRequestBody(
+        email: emailController.text,
+        password: passwordController.text,
+      ),
+    );
+    response.when(success: (loginResponse) async {
+      await saveUserToken(loginResponse.userData?.token ?? '');
+      emit(LoginState.success(loginResponse));
+    }, failure: (error) {
+      emit(LoginState.error(error: error.apiErrorModel.message ?? ''));
+    });
+  }
+
+  Future<void> saveUserToken(String token) async {
+    await SharedPrefHelper.setSecuredString(SharedPrefKeys.userToken, token);
+    DioFactory.setTokenIntoHeaderAfterLogin(token);
+  }
 }
