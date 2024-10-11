538122ff82b4d20bee1894a8ee3f2b7ec1f2f4e9
asma <asmaamagdy030@gmail.com>
2024-10-11 19:46:32 +0200

    change the di

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,4 +1,7 @@
 import 'package:diva/core/networking/dio_factory.dart';
+import 'package:diva/features/login/data/apis/login_api_service.dart';
+import 'package:diva/features/login/data/repo/login_repo.dart';
+import 'package:diva/features/login/logic/login_cubit.dart';
 import 'package:diva/features/signup/data/apis/signup_api_service.dart';
 import 'package:diva/features/signup/data/repos/signup_repos.dart';
 import 'package:diva/features/signup/logic/signup_cubit.dart';
@@ -9,6 +12,10 @@ final getIt = GetIt.instance;
 void setupGetIt() {
   // Dio
   final dio = DioFactory.getDio();
+  getIt.registerLazySingleton<LoginApiService>(() => LoginApiService(dio));
+// login
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
 
   // Signup
   getIt
